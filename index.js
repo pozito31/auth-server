25ed5ffe7557fe0f3ee0ed96399c4569b2898644
Jessica Pozo <jessicapozo198715@gmail.com>
2022-04-23 14:08:17 +0200

    agregando cambios

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
 // Crear el servidor/aplicación de express
